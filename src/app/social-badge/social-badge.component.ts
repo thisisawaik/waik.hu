558ef63e94c2b911c6644ad52ca95daf8c524ed9
Orosfai I. Zsolt <orosfai11@gmail.com>
2021-06-04 04:22:57 +0200

    Type in social-badge

--- src/app/social-badge/social-badge.component.ts
+++ src/app/social-badge/social-badge.component.ts
@@ -15,36 +15,41 @@ export class SocialBadgeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
   public get socialUrl() : string {
-    const platforms = {
-      'youtube'  : (user: string) => `https://www.youtube.com/user/${user}`,
-      'twitch'   : (user: string) => `https://www.twitch.tv/${user}`,
-      'twitter'  : (user: string) => `https://twitter.com/${user}`,
-      'github'   : (user: string) => `https://github.com/${user}`,
-      'facebook' : (user: string) => `https://facebook.com/${user}`,
-      'instagram': (user: string) => `https://instagram.com/${user}`,
-      'reddit'   : (user: string) => `https://www.reddit.com/${user}`,
-      'vimeo'    : (user: string) => `https://vimeo.com/${user}`,
+    const platforms : SocialFunc = {
+      'youtube'  : (user) => `https://www.youtube.com/user/${user}`,
+      'twitch'   : (user) => `https://www.twitch.tv/${user}`,
+      'twitter'  : (user) => `https://twitter.com/${user}`,
+      'github'   : (user) => `https://github.com/${user}`,
+      'facebook' : (user) => `https://facebook.com/${user}`,
+      'instagram': (user) => `https://instagram.com/${user}`,
+      'reddit'   : (user) => `https://www.reddit.com/${user}`,
+      'vimeo'    : (user) => `https://vimeo.com/${user}`,
     }
 
-    return `${platforms[this.socialType](this.socialUser)}?utm_source=waik.hu`
+    if(this.socialUser) return `${platforms[this.socialType](this.socialUser)}?utm_source=waik.hu`;
+    else return `/`;
   }
 
-
   public get socialIcon() : string {
-    switch (this.socialType) {
-      case 'youtube': return `assets/svgs/yt_logo.svg`;
-      case 'twitch': return `assets/svgs/twitch_logo.svg`;
-      case 'twitter': return `assets/svgs/twitter_logo.svg`;
-      case 'github': return `assets/svgs/github_logo.svg`;
-      case 'facebook': return `assets/svgs/facebook_logo.svg`;
-      case 'instagram': return `assets/svgs/instagram_logo.svg`;
-      case 'reddit': return `assets/svgs/reddit_logo.svg`;
-      case 'vimeo': return `assets/svgs/vimeo_logo.svg`;
-      default: return'null';
+    const platforms : SocialFunc = {
+      'youtube'  : () => `yt_logo.svg`,
+      'twitch'   : () => `twitch_logo.svg`,
+      'twitter'  : () => `twitter_logo.svg`,
+      'github'   : () => `github_logo.svg`,
+      'facebook' : () => `facebook_logo.svg`,
+      'instagram': () => `instagram_logo.svg`,
+      'reddit'   : () => `reddit_logo.svg`,
+      'vimeo'    : () => `vimeo_logo.svg`,
     }
+
+    if(this.socialUser) return `assets/svgs/${platforms[this.socialType](this.socialUser)}`
+    else return `assets/svgs/no-found-icon.svg`;
   }
 }
 
 type SocialType = 'youtube' | 'twitch' | 'twitter' | 'github' | 'facebook' | 'instagram' | 'reddit' | 'vimeo';
+
+type SocialFunc = {
+  [name in SocialType]:  (user?: string) => string;
+};
