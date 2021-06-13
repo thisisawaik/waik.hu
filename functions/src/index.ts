b199341cfbb05b5cad4bc7346e8ca535cc0ba2c2
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-13 16:31:29 +0200

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
+/* eslint-disable require-jsdoc */
+
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import vision from "@google-cloud/vision";
 
+const client = new vision.ImageAnnotatorClient();
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -14,34 +18,34 @@ const db = admin.firestore();
 // });
 
 export const fstest = functions.firestore
-  .document("test/YWjeaGh8R7RpKhHOXyov")
-  .onWrite((change) => {
-    console.log(change.after);
-    console.log(change.before);
-    return;
-  });
+    .document("test/YWjeaGh8R7RpKhHOXyov")
+    .onWrite((change) => {
+      console.log(change.after);
+      console.log(change.before);
+      return;
+    });
 
 export const waikFanartAddLike = functions.https.onCall((data, context) => {
-  //console.log(context);
-  //if (context.app == undefined) {
+  // console.log(context);
+  // if (context.app == undefined) {
   //  throw new functions.https.HttpsError(
   //      'failed-precondition',
   //      'The function must be called from an App Check verified app.'
   //      );
-  //}
+  // }
   const uid = context.auth?.uid;
   if (!uid) {
     throw new functions.https.HttpsError(
-      "unauthenticated",
-      "user-not-provided"
+        "unauthenticated",
+        "user-not-provided"
     );
   }
 
   const id = data.postId;
   if (!id) {
     throw new functions.https.HttpsError(
-      "invalid-argument",
-      "post-id-not-provided"
+        "invalid-argument",
+        "post-id-not-provided"
     );
   }
   const postref = db.doc(`waik/website/fanarts/${id}`);
@@ -56,40 +60,36 @@ export const waikFanartAddLike = functions.https.onCall((data, context) => {
 
     if (isLiked) {
       throw new functions.https.HttpsError(
-        "permission-denied",
-        "already-liked"
+          "permission-denied",
+          "already-liked"
       );
     }
     likes.push(uid);
 
     postref
-      .update({
-        likes: likes,
-      })
-      .then((res) => {
-        return {
-          status: "OK",
+        .update({
           likes: likes,
-        };
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError("internal", "internal-error");
-      });
+        })
+        .then((res) => {
+          return {
+            status: "OK",
+            likes: likes,
+          };
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError("internal", "internal-error");
+        });
   });
 });
 
 export const waikFanartSubmit = functions.https.onCall((data, context) => {
-  const vision = require("@google-cloud/vision");
-
-  const client = new vision.ImageAnnotatorClient();
-
-  console.log(data)
+  console.log(data);
 
   const uid = context.auth?.uid;
   if (!uid) {
     throw new functions.https.HttpsError(
-      "unauthenticated",
-      "user-not-provided"
+        "unauthenticated",
+        "user-not-provided"
     );
   }
   const id = data.postId;
@@ -100,52 +100,56 @@ export const waikFanartSubmit = functions.https.onCall((data, context) => {
   const ref = db.doc(`waik/website/fanarts/${id}`);
 
   ref.get().then(async (doc) => {
-    if (!doc.exists)
+    if (!doc.exists) {
       throw new functions.https.HttpsError(
-        "not-found",
-        `fanart-not-found-${id}`
+          "not-found",
+          `fanart-not-found-${id}`
       );
+    }
 
-    if (!doc.data()?.gsURL)
-      throw new functions.https.HttpsError("not-found", `gs-url-not-found`);
+    if (!doc.data()?.gsURL) {
+      throw new functions.https.HttpsError("not-found", "gs-url-not-found");
+    }
 
     const [result] = await client.safeSearchDetection(`gs://zal1000.net${doc.data()?.gsURL}`);
     const detections = result.safeSearchAnnotation;
 
     console.log("Safe search:");
-    console.log(`Adult: ${detections.adult}`);
-    console.log(`Medical: ${detections.medical}`);
-    console.log(`Spoof: ${detections.spoof}`);
-    console.log(`Violence: ${detections.violence}`);
-    console.log(`Racy: ${detections.racy}`);
+    console.log(`Adult: ${detections?.adult}`);
+    console.log(`Medical: ${detections?.medical}`);
+    console.log(`Spoof: ${detections?.spoof}`);
+    console.log(`Violence: ${detections?.violence}`);
+    console.log(`Racy: ${detections?.racy}`);
+
+    console.log(detections);
 
     return {
       detections: detections,
-    }
+    };
   });
 });
 
 export const waikFanartAddRemove = functions.https.onCall((data, context) => {
-  //console.log(context);
-  //if (context.app == undefined) {
+  // console.log(context);
+  // if (context.app == undefined) {
   //  throw new functions.https.HttpsError(
   //      'failed-precondition',
   //      'The function must be called from an App Check verified app.'
   //      );
-  //}
+  // }
   const uid = context.auth?.uid;
   if (!uid) {
     throw new functions.https.HttpsError(
-      "unauthenticated",
-      "user-not-provided"
+        "unauthenticated",
+        "user-not-provided"
     );
   }
 
   const id = data.postId;
   if (!id) {
     throw new functions.https.HttpsError(
-      "invalid-argument",
-      "post-id-not-provided"
+        "invalid-argument",
+        "post-id-not-provided"
     );
   }
   const postref = db.doc(`waik/website/fanarts/${id}`);
@@ -160,30 +164,30 @@ export const waikFanartAddRemove = functions.https.onCall((data, context) => {
 
     if (!isLiked) {
       throw new functions.https.HttpsError(
-        "permission-denied",
-        "not-yet-liked"
+          "permission-denied",
+          "not-yet-liked"
       );
     }
     removeElement(likes, uid);
 
     postref
-      .update({
-        likes: likes,
-      })
-      .then((res) => {
-        return {
-          status: "OK",
+        .update({
           likes: likes,
-        };
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError("internal", "internal-error");
-      });
+        })
+        .then((res) => {
+          return {
+            status: "OK",
+            likes: likes,
+          };
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError("internal", "internal-error");
+        });
   });
 });
 
 function removeElement(array: Array<string>, elem: string) {
-  var index = array.indexOf(elem);
+  const index = array.indexOf(elem);
   if (index > -1) {
     array.splice(index, 1);
   }
