08b7deef189d102eb5b3f7d93bd17df2f2f59192
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-19 21:00:45 +0200

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,22 @@ const db = admin.firestore();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get('/ping', (req, res) => {
+    res.status(200).send('PONG')
+});
 
+app.get('/users/dc/:DcId', async (req, res) => {
+    const id = req.params.DcId;
+    if(!id) return res.status(400).send('id-not-found');
+    await db.collection('dcusers').doc(id).get().then(doc => {
+        if(!doc.exists) return res.status(404).send('user-not-found');
+        return res.status(200).send(doc.data());
+    }).catch(e => {
+        return res.sendStatus(500);
+    });
+
+    return res.sendStatus(500);
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
