3a2439d3db644b400e49bd7ff89bbd28a57f7832
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-19 21:38:14 +0200

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -25,7 +25,7 @@ app.get('/users/dc/:DcId', async (req, res) => {
 });
 
 app.get('/share', async (req, res) => {
-    res.render('<html><head><meta name="twitter:card" content="summary"><meta name="twitter:title" content="Test"><meta name="twitter:site" content="waik.hu"><meta name="twitter:description" content="This is a longer text couse yeah"><meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/zal1000.net/o/images%2FUntitled-2.jpg?alt=media&token=a74e466f-e0c6-4eff-b366-4ef5e507c9e3"></head></html>')
+    res.render('./html/index.html')
 })
 
 app.listen(port, () => {
