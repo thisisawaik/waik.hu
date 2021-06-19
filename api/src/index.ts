4c65b6469a093137c351dd811cc529e455c6ed44
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-19 22:01:52 +0200

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -36,15 +36,21 @@ app.get("/share", async (req, res) => {
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="Test" />
     <meta name="twitter:site" content="waik.hu" />
-    <meta
-      name="twitter:description"
-      content="This is a longer text couse yeah"
-    />
+    <meta name="twitter:description" content="This is a longer text couse yeah" />
+    <meta name="og:image" content="https://firebasestorage.googleapis.com/v0/b/zal1000.net/o/images%2FUntitled-2.jpg?alt=media&token=a74e466f-e0c6-4eff-b366-4ef5e507c9e3" />
+    <meta name="og:title" content="Test" />
+    <meta name="og:description" content="This is a longer text couse yeah" />
+
     <meta
       name="twitter:image"
       content="https://firebasestorage.googleapis.com/v0/b/zal1000.net/o/images%2FUntitled-2.jpg?alt=media&token=a74e466f-e0c6-4eff-b366-4ef5e507c9e3"
     />
   </head>
+  <body>
+    <sctipt>
+        window.location = "https://example.com/";
+    </script>
+    </body>
 </html>
 `);
 });
