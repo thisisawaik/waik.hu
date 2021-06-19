8832d6279c5bb6b940aa33e4353b541ecec5a23a
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-19 21:44:33 +0200

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
-import * as express from 'express';
-import * as admin from 'firebase-admin';
+import * as express from "express";
+import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
@@ -7,27 +7,48 @@ const db = admin.firestore();
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.status(200).send('PONG')
+app.get("/ping", (req, res) => {
+  res.status(200).send("PONG");
 });
 
-app.get('/users/dc/:DcId', async (req, res) => {
-    const id = req.params.DcId;
-    if(!id) return res.status(400).send('id-not-found');
-    await db.collection('dcusers').doc(id).get().then(doc => {
-        if(!doc.exists) return res.status(404).send('user-not-found');
-        return res.status(200).send(doc.data());
-    }).catch(e => {
-        return res.sendStatus(500);
+app.get("/users/dc/:DcId", async (req, res) => {
+  const id = req.params.DcId;
+  if (!id) return res.status(400).send("id-not-found");
+  await db
+    .collection("dcusers")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) return res.status(404).send("user-not-found");
+      return res.status(200).send(doc.data());
+    })
+    .catch((e) => {
+      return res.sendStatus(500);
     });
 
-    return res.sendStatus(500);
+  return res.sendStatus(500);
 });
 
-app.get('/share', async (req, res) => {
-    res.render('./html/index.html')
-})
+app.get("/share", async (req, res) => {
+  res.status(200).send(`
+    <html>
+  <head>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Test" />
+    <meta name="twitter:site" content="waik.hu" />
+    <meta
+      name="twitter:description"
+      content="This is a longer text couse yeah"
+    />
+    <meta
+      name="twitter:image"
+      content="https://firebasestorage.googleapis.com/v0/b/zal1000.net/o/images%2FUntitled-2.jpg?alt=media&token=a74e466f-e0c6-4eff-b366-4ef5e507c9e3"
+    />
+  </head>
+</html>
+`);
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
