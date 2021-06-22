d37ad0aeea650baca2bfe306a940b2b84d28f5fb
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-23 01:13:35 +0200

    Add mass role progress

--- web/src/app/admin/admin.component.ts
+++ web/src/app/admin/admin.component.ts
@@ -15,6 +15,14 @@ export class AdminComponent implements OnInit {
     finishedAt: "Loading...",
   }
 
+  massrole = {
+    all: 0,
+    done: 0,
+    isRunning: false,
+    startedAt: "Loading...",
+    finishedAt: "Loading...",
+  }
+
   rdb = getDatabase();
 
   constructor() { }
@@ -26,6 +34,14 @@ export class AdminComponent implements OnInit {
       this.rolesync.isRunning = res.val().running
       this.rolesync.startedAt = res.val().startedAt ? new Date(res.val().startedAt).toUTCString() : "Not yet started";
       this.rolesync.finishedAt = res.val().finishedAt ? new Date(res.val().finishedAt).toUTCString() : "Not yet finished";
+    })
+
+    onValue(refFromURL(this.rdb, `https://waik.europe-west1.firebasedatabase.app/admin/massadd`), (res) => {
+      this.massrole.all = res.val().all
+      this.massrole.done = res.val().done
+      this.massrole.isRunning = res.val().running
+      this.massrole.startedAt = res.val().startedAt ? new Date(res.val().startedAt).toUTCString() : "Not yet started";
+      this.massrole.finishedAt = res.val().finishedAt ? new Date(res.val().finishedAt).toUTCString() : "Not yet finished";
     })
   }
 
