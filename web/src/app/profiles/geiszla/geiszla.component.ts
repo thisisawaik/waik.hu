486994cd9f06e5c38ebc5f033c3f63e4a14d4273
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-26 05:45:05 +0200

    Create geiszla.component.ts

---
+++ web/src/app/profiles/geiszla/geiszla.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geiszla',
+  templateUrl: './geiszla.component.html',
+  styleUrls: ['./geiszla.component.scss']
+})
+export class GeiszlaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
