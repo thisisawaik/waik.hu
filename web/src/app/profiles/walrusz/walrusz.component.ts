75c98f16e6b48c971bfa66b3a6d899286748e9bb
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-26 05:12:50 +0200

    Added description card from Walrusz
    The description is currently from twitch. Feel free to change it @Walrusz
    
    Co-Authored-By: Walrusz <8427779+walrusz@users.noreply.github.com>

--- web/src/app/profiles/walrusz/walrusz.component.ts
+++ web/src/app/profiles/walrusz/walrusz.component.ts
@@ -98,6 +98,22 @@ export class WalruszComponent implements OnInit {
     );
   }
 
+  detectMob(): boolean {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+  }
+
   // GET https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={CHANNEL_ID}&maxResults=10&order=date&type=video&key={YOUR_API_KEY}
 }
 
