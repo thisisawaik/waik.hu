a55e81fd145e90adacdc1ac1c9c517445271ad02
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-26 05:11:45 +0200

    Added description card for Isti
    The description is currently from twitch. Feel free to change it @Isti115
    
    Co-Authored-By: István Donkó <istvan.donko+github@gmail.com>

--- web/src/app/profiles/isti/isti.component.ts
+++ web/src/app/profiles/isti/isti.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 @Component({
   selector: 'app-isti',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./isti.component.scss']
 })
 export class IstiComponent implements OnInit {
+  name: string = "Isti115"
+  pp: string = "https://cdn.discordapp.com/avatars/174980450543075330/68bd161e38aaaff31a57ed05bf4ea971.webp";
+
+  db = getFirestore();
 
   constructor() { }
 
   ngOnInit(): void {
+    const tdrRef = doc(this.db, 'dcusers/174980450543075330');
+
+    getDoc(tdrRef).then(res => {
+      this.pp = res.data()?.pp ? res.data()?.pp : this.pp;
+      this.name = res.data()?.username ? res.data()?.username : this.name;
+    })
   }
 
 }
