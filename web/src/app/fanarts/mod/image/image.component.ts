ad3a0a1a75eed7045600aa140ce4032aaac3a4c2
Zalán Horváth <horvathzalan@zal1000.com>
2021-06-29 11:54:44 +0200

    Update image.component.ts

--- web/src/app/fanarts/mod/image/image.component.ts
+++ web/src/app/fanarts/mod/image/image.component.ts
@@ -17,21 +17,14 @@ import { getApp } from '@firebase/app';
 @Component({
   selector: 'app-mod-image',
   templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  styleUrls: ['./image.component.scss'],
 })
 export class ModImageComponent implements OnInit, OnDestroy {
-
-  liked: boolean = false;
-  shareable: boolean = false;
-  shareclass: string = 'red';
-  sharetext: string = 'Megosztás';
   //shareid: string | undefined;
   authorAvatar: string | undefined;
   authorName: string | undefined;
-  likes: number = 0;
-  submitclass: string = 'red';
-  title: string = "Loading..."
-  desc: string = "Loading..."
+  title: string = 'Loading...';
+  desc: string = 'Loading...';
 
   greenbackground: boolean = false;
 
@@ -39,8 +32,6 @@ export class ModImageComponent implements OnInit, OnDestroy {
   auth = getAuth();
   funcions = getFunctions();
   rdb = getDatabase();
-
-  likeListener: any;
 
   constructor(
     public dialog: MatDialog,
@@ -58,17 +49,12 @@ export class ModImageComponent implements OnInit, OnDestroy {
   @Input() shareid: string | undefined;
 
   async ngOnInit(): Promise<void> {
-    getDoc(doc(this.db, `waik/website/fanarts/${this.id}`)).then(res => {
-      this.title = res.data()?.title ? res.data()?.title : "Cím";
-      this.desc = res.data()?.desc ? res.data()?.desc : "Leírás";
+    getDoc(doc(this.db, `waik/website/fanarts/${this.id}`)).then((res) => {
+      this.title = res.data()?.title ? res.data()?.title : 'Cím';
+      this.desc = res.data()?.desc ? res.data()?.desc : 'Leírás';
     });
-    if (this.shareid) {
-      this.shareable = true;
-    }
-    console.log(this.getFromGS)
     if (this.getFromGS && this.gsurl) {
       this.imageurl = await firebase.storage().ref(this.gsurl).getDownloadURL();
-      console.log(await firebase.storage().ref(this.gsurl).getDownloadURL());
     }
     if (this.author) {
       const userref = doc(this.db, `users/${this.author}`);
@@ -85,101 +71,46 @@ export class ModImageComponent implements OnInit, OnDestroy {
         })
         .catch((e) => this.msg.error(e.message));
     }
-    const likesref = ref(this.rdb, `fanarts/${this.id}`);
-    this.likeListener = onValue(likesref, snap => {
-      //console.log(snap.val())
-      this.likes = snap.val().likes;
-    })
     const user = this.auth.currentUser;
-    const likeRef = doc(
-      this.db,
-      `waik/website/fanarts/${this.id}/likes/${user?.uid}`
-    );
-    const likeDoc = await getDoc(likeRef);
-    if (user && likeDoc.exists()) {
-      this.liked = true;
-    }
   }
 
   ngOnDestroy() {
-    this.likeListener();
+    // this.likeListener();
   }
 
-  open() {
-    const dialogref = this.dialog.open(ImageDialogComponent, {
-      data: {
-        id: this.id,
-      },
-    });
-
-    dialogref.afterClosed().subscribe((res) => {
-      console.log(res);
-    });
-
+  async approve() {
+    const user = this.auth.currentUser;
+    httpsCallable(
+      this.funcions,
+      'waikFanartApprove'
+    )({ postId: this.id })
+      .then((res) => {
+        //console.log(res);
+        this.msg.success('Sikeres jóváhagyás!');
+      })
+      .catch((e) => {
+        this.msg.error(`Hiba jóváhagyás közben! (${e.message})`);
+        console.error(e);
+      });
   }
 
-  async like() {
+  async deny() {
     const user = this.auth.currentUser;
     if (!user) {
       this.dialog.open(AccountComponent, {
         minWidth: 600,
       });
     } else {
-      this.liked = true;
-      this.likes = this.likes + 1;
-      httpsCallable(
-        this.funcions,
-        'waikFanartAddLike'
-      )({ postId: this.id })
-        .then((res) => {
-          //console.log(res);
-          //this.msg.success('Like hozzáadva!');
-        })
-        .catch((e) => {
-          this.msg.error(`Hiba like-olás közben! (${e.message})`);
-          console.error(e);
-          this.liked = false;
-          this.likes = this.likes - 1;
-
-        });
-      /*
-      const token = await this.authServ.getAuthToken();
-      this.liked = true;
-      this.http
-        .post(
-          `${environment.apiUrl}/like/${this.id}?uid=${user?.uid}&token=${token}`,
-          {}
-        )
-        .subscribe(
-          (res) => {
-            this.msg.success('Like hozzáadva!');
-          },
-          (err) => {
-            this.msg.error(`Hiba like-olás közben! (${err.message})`);
-            this.liked = false;
-          }
-        );
-      */
-    }
-  }
-
-  async dislike() {
-    const user = this.auth.currentUser;
-    if (!user) {
-      this.dialog.open(AccountComponent, {
-        minWidth: 600,
-      });
-    } else {
-      this.liked = false;
       httpsCallable(
         this.funcions,
         'waikFanartLikeRemove'
       )({ postId: this.id })
         .then((res) => {
+          this.msg.success('Sikeres elutasítás!');
+
         })
         .catch((err) => {
-          this.msg.error(`Hiba like visszavonása közben! (${err.message})`);
-          this.liked = true;
+          this.msg.error(`Hiba elutasítás közben! (${err.message})`);
         });
     }
   }
@@ -190,13 +121,8 @@ export class ModImageComponent implements OnInit, OnDestroy {
     }/share/${this.shareid}`;
     this.clipboard.copy(link);
     this.msg.success(`Link másolva a vágólapra! (${link})`);
-    this.shareclass = 'green';
-    this.sharetext = 'Link a vágólapon';
 
-    setTimeout(() => {
-      this.shareclass = 'red';
-      this.sharetext = 'Megosztás';
-    }, 5000);
+    setTimeout(() => {}, 5000);
   }
 
   removeElement(array: Array<string>, elem: string) {
@@ -208,18 +134,17 @@ export class ModImageComponent implements OnInit, OnDestroy {
 
   detectMob() {
     const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
     ];
 
     return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
+      return navigator.userAgent.match(toMatchItem);
     });
   }
-
 }
