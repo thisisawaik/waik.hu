1d74fc660fafc3819460b73cca70b07d7f5ca23c
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-02 01:00:08 +0200

    Update server.ts

--- web/server.ts
+++ web/server.ts
@@ -30,7 +30,9 @@ export function app(): express.Express {
   server.set('views', distFolder);
 
   // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
+  server.get('/api/shares/ShareId', (req, res) => {
+    res.status(200).send({status: 'OK'});
+  });
   // Serve static files from /browser
   server.get('*.*', express.static(distFolder, {
     maxAge: '1w'
