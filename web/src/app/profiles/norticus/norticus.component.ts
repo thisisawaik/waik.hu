81781ee655337ac0bac6caa03cef2debe7b27d5e
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-01 20:25:12 +0200

    Update norticus.component.ts

--- web/src/app/profiles/norticus/norticus.component.ts
+++ web/src/app/profiles/norticus/norticus.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 @Component({
   selector: 'app-norticus',
   templateUrl: './norticus.component.html',
-  styleUrls: ['./norticus.component.scss']
+  styleUrls: ['./norticus.component.scss'],
 })
 export class NorticusComponent implements OnInit {
   db = getFirestore();
-  pp: string = "https://cdn.discordapp.com/avatars/118466559738904576/6b4610199259efab8493cafb3e049938.webp";
-  name: string = "Norticus36#7832";
-  
-  desc: string  = `Sziasztok! Norticus (Todi) vagyok, főként számítógépes játékokkal és
+  pp: string =
+    'https://cdn.discordapp.com/avatars/118466559738904576/6b4610199259efab8493cafb3e049938.webp';
+  name: string = 'Norticus36#7832';
+
+  desc: string = `Sziasztok! Norticus (Todi) vagyok, főként számítógépes játékokkal és
   az informatikával foglalkozom. Néha talán túlságosan is versengő
   vagyok a mentális jólétemnek. Ha játékokról vagy animékről van szó,
   szívesen elbeszélgetek róluk. Jelenleg informatikushallgató vagyok, de
@@ -21,16 +23,32 @@ export class NorticusComponent implements OnInit {
   a szervereket amin a csapat játszik legtöbbször én hosztolom.
   Igyekszem élőben is közvetíteni a történéseket, ha új sorozat készül
   éppen, akkor én (és néha Isti) streameljük élőben, de szoktam egyedül
-  is streamelni, akár angolul is.`
-  constructor() { }
+  is streamelni, akár angolul is.`;
+  constructor(private htmltitle: Title, private meta: Meta) {}
 
   ngOnInit(): void {
-
+    this.htmltitle.setTitle('Waik | Norticus36')
+    this.meta.addTag({ property: 'og:title', content: 'Waik | Norticus36' });
+    this.meta.addTag({
+      property: 'og:image',
+      content: this.pp,
+    });
+    this.meta.addTag({
+      property: 'og:description',
+      content: this.desc,
+    });
+    this.meta.addTag({ property: 'twitter:site', content: 'https://waik.hu/' });
+    this.meta.addTag({
+      property: 'twitter:creator',
+      content: '@zal1000original',
+    });
+    this.meta.addTag({ property: 'twitter:image:alt', content: 'Waik' });
+    this.meta.addTag({ property: 'fb:app_id', content: '581458672492860' });
+    this.meta.addTag({ property: 'og:url', content: 'https://waik.hu/' });
     const tdrRef = doc(this.db, 'dcusers/118466559738904576');
 
-    getDoc(tdrRef).then(res => {
-      this.pp = res.data()?.pp
-    })
+    getDoc(tdrRef).then((res) => {
+      this.pp = res.data()?.pp;
+    });
   }
-
 }
