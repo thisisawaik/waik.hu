f4dcb1906efb9a0b9486265764d583b4e6d0efa8
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-07 04:18:31 +0200

    Added navbar buttons

--- web-vue/layouts/default.vue
+++ web-vue/layouts/default.vue
@@ -1,41 +1,12 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-app-bar :clipped-left="clipped" fixed app >
+      <NuxtLink class="nav-bar-button" to="/"><img width="40" alt="Waik Logo" src="~/assets/waik_big_icon.png" style="cursor: pointer;"/></NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/"><v-btn color="primary" elevation="2">Főoldal</v-btn></NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/streams"><v-btn color="primary" elevation="2">Streamek</v-btn></NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/downloads"><v-btn color="primary" elevation="2">Letöltések</v-btn></NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/fanarts"><v-btn color="primary" elevation="2">Fanartok</v-btn></NuxtLink>
+      <NuxtLink class="nav-bar-button" to="/admin"><v-btn color="primary" elevation="2">Admin</v-btn></NuxtLink>
       <v-spacer />
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
@@ -90,3 +61,9 @@ export default {
   },
 }
 </script>
+
+<style>
+  .nav-bar-button {
+    margin-right: 15px;
+  }
+</style>
