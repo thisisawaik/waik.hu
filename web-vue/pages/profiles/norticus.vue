110e00cfa60b8a98bafe9d24ad5606bffb9292a1
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-07 05:48:09 +0200

    Update norticus.vue

--- web-vue/pages/profiles/norticus.vue
+++ web-vue/pages/profiles/norticus.vue
@@ -33,6 +33,7 @@ export default {
       dctag: 'Norticus36#7832',
       color: '#278003',
       desc: 'Sziasztok! Norticus (Todi) vagyok, főként számítógépes játékokkal és az informatikával foglalkozom. Néha talán túlságosan is versengő vagyok a mentális jólétemnek. Ha játékokról vagy animékről van szó, szívesen elbeszélgetek róluk. Jelenleg informatikushallgató vagyok, de igyekszem időt szánni magamra és a streamelésre is. A waik csapat egyik alapító tagja vagyok (fun fact, az eredeti waik figurához egyedül én nem rajzoltam hozzá, mire odaértem már véglegesítve lett), a szervereket amin a csapat játszik legtöbbször én hosztolom. Igyekszem élőben is közvetíteni a történéseket, ha új sorozat készül éppen, akkor én (és néha Isti) streameljük élőben, de szoktam egyedül is streamelni, akár angolul is.',
+      loading: false,
     }
   },
   head() {
@@ -42,6 +43,10 @@ export default {
         {
           name: 'og:title',
           content: `Waik | ${this.name}`,
+        },
+        {
+          name: 'description',
+          content: this.desc,
         },
         {
           name: 'og:description',
