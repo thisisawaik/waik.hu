170e6014bc923d92401ddca66ad8ddbd2267582a
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-07 14:59:43 +0200

    Update walrusz.vue

--- web-vue/pages/profiles/walrusz.vue
+++ web-vue/pages/profiles/walrusz.vue
@@ -30,7 +30,7 @@ export default {
       name: 'Walrusz',
       imageurl:
         'https://yt3.ggpht.com/ytc/AAUvwniseLALwR5fmrqhrt2YDGgh1VJSCviENV2MkukhUg=s68-c-k-c0x00ffffff-no-rj',
-      dctag: 'Norticus36#1019',
+      dctag: 'Walrusz#1019',
       color: '#7f5b26',
       desc: `Üdvözöllek! Walrusz vagyok és úgy hiszem, bármiről lehet érdekes, kreatív és szórakoztató videót készíteni, hogyha az ember elegendő munkát fektet bele. Mesterségem elsősorban a szórakoztató ismeretterjesztő tartalmak gyártása, de emellett sok más mindent is találhatsz a csatornámon. Szeretek nagy hangsúlyt fektetni a videók vágására, az utómunkára és hiszek abban, hogy a minőségi szórakoztatás kulcsa a kreativitásban rejlik. Bátran nézz szét a csatornámon, háthatalálsz valamit, ami tetszik :)`,
       loading: false,
