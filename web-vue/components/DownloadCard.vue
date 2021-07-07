2e04c21d09bdbe1a2e426d00f5b064f21c877cfe
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-07 19:25:01 +0200

    Update DownloadCard.vue

--- web-vue/components/DownloadCard.vue
+++ web-vue/components/DownloadCard.vue
@@ -7,15 +7,9 @@
         indeterminate
       ></v-progress-linear>
     </template>
-    <v-card-title>{{title}}</v-card-title>
-    <v-img
-      v-if="imageurl"
-      height="250"
-      :src="imageurl"
-    ></v-img>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-img v-if="imageurl" height="250" :src="imageurl"></v-img>
     <v-card-text>
-
-
       <div>{{ desc }}</div>
     </v-card-text>
 
@@ -33,7 +27,9 @@
         </v-list-item-avatar>
 
         <v-list-item-content v-if="loading || authorName">
-          <v-list-item-title>{{ authorName === "Loading..." ? " " : authorName }}</v-list-item-title>
+          <v-list-item-title>{{
+            authorName === 'Loading...' ? ' ' : authorName
+          }}</v-list-item-title>
         </v-list-item-content>
 
         <v-row align="center" justify="end">
@@ -44,12 +40,13 @@
       </v-list-item>
     </v-card-actions>
   </v-card>
+
 </template>
 
 <script>
 export default {
-    // eslint-disable-next-line vue/require-prop-types
-    props: [ 'id' ],
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['id'],
   data() {
     return {
       loading: true,
@@ -57,41 +54,50 @@ export default {
       desc: 'Loading...',
       imageurl: null,
       authorAvatar: null,
-      authorName: "Loading...",
+      authorName: 'Loading...',
       downloadurl: null,
       githuburl: null,
+      showCopyBar: false,
     }
   },
   async created() {
-      const db = this.$fire.firestore
-      // const storage = this.$fire.storage
-      const ref = db.collection('waik/website/downloads').doc(this.id)
-      try {
-        const doc = await ref.get()
-        this.title = doc.data().name
-        this.desc = doc.data().desc
-        if(doc.data().imageurl) {
-            // const sref = storage.ref(doc.data().gsURL)
-            // this.imageurl = await sref.getDownloadURL()
-            this.imageurl = doc.data().imageurl
-
-        }
-        if(doc.data().author) {
-            const dcRef = db.collection('dcusers').doc(doc.data().author)
-                const dcDoc = await dcRef.get()
-                this.authorAvatar = dcDoc.data().pp
-                this.authorName = dcDoc.data().tag
-        }
-        this.loading = false
-
-      } catch (e) {
-          // eslint-disable-next-line no-console
-          console.error(e)
-          this.loading = false
-
+    const db = this.$fire.firestore
+    // const storage = this.$fire.storage
+    const ref = db.collection('waik/website/downloads').doc(this.id)
+    try {
+      const doc = await ref.get()
+      this.title = doc.data().name
+      this.desc = doc.data().desc
+      if (doc.data().imageurl) {
+        // const sref = storage.ref(doc.data().gsURL)
+        // this.imageurl = await sref.getDownloadURL()
+        this.imageurl = doc.data().imageurl
       }
-
-  }
+      if (doc.data().author) {
+        const dcRef = db.collection('dcusers').doc(doc.data().author)
+        const dcDoc = await dcRef.get()
+        this.authorAvatar = dcDoc.data().pp
+        this.authorName = dcDoc.data().tag
+      }
+      this.loading = false
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e)
+      this.loading = false
+    }
+  },
+  methods: {
+    copyShareUrl() {},
+    download() {},
+    openGithub() {},
+    async copySomething(text) {
+      try {
+        await this.$copyText(text)
+      } catch (e) {
+        // console.error(e);
+      }
+    },
+  },
 }
 </script>
 
