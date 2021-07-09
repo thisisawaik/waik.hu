3fb1ede2aeacb605f26073f5df2e8a30ea68f96e
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-09 21:24:32 +0200

    Update SocialBar.vue

--- web-vue/components/SocialBar.vue
+++ web-vue/components/SocialBar.vue
@@ -1,39 +1,75 @@
 <template>
   <v-app-bar style="margin-top: 60px; height: 70px" fixed app color="#1e1e1e">
     <div v-if="user === 'norticus'" class="center">
-      <a href="https://www.twitch.tv/norticus36" target="_blank"><img height="40" src="~/assets/svgs/twitch_logo.svg" /></a>
-      <a href="https://www.youtube.com/user/NorticusGaming" target="_blank"><img height="40" src="~/assets/svgs/yt_logo.svg" /></a>
-      <a href="https://github.com/norticus36/" target="_blank"><img height="40" src="~/assets/svgs/github_logo.svg" /></a>
-      <a><img height="40" class="avatar" src="~/assets/avatars/norticus.webp" /></a>
+      <a href="https://www.twitch.tv/norticus36" target="_blank"
+        ><img height="40" src="~/assets/svgs/twitch_logo.svg"
+      /></a>
+      <a href="https://www.youtube.com/user/NorticusGaming" target="_blank"
+        ><img height="40" src="~/assets/svgs/yt_logo.svg"
+      /></a>
+      <a href="https://github.com/norticus36/" target="_blank"
+        ><img height="40" src="~/assets/svgs/github_logo.svg"
+      /></a>
+      <a
+        ><img height="40" class="avatar" src="~/assets/avatars/norticus.webp"
+      /></a>
+    </div>
+    <div v-if="user === 'isti'" class="center">
+      <a href="https://www.twitch.tv/Isti115" target="_blank"
+        ><img height="40" src="~/assets/svgs/twitch_logo.svg"
+      /></a>
+      <a href="https://twitter.com/Isti115/" target="_blank"
+        ><img height="40" src="~/assets/svgs/twitter_logo.svg"
+      /></a>
+      <a href="https://www.youtube.com/user/Isti115/" target="_blank"
+        ><img height="40" src="~/assets/svgs/yt_logo.svg"
+      /></a>
+      <a href="https://github.com/Isti115/" target="_blank"
+        ><img height="40" src="~/assets/svgs/github_logo.svg"
+      /></a>
+      <a
+        ><img height="40" class="avatar" src="~/assets/avatars/isti.webp"
+      /></a>
+    </div>
+        <div v-if="user === 'walrusz'" class="center">
+          <a href="https://www.youtube.com/user/WalruszMester/" target="_blank"
+        ><img height="40" src="~/assets/svgs/yt_logo.svg"
+      /></a>
+      <a href="https://twitter.com/walruszmester/" target="_blank"
+        ><img height="40" src="~/assets/svgs/twitter_logo.svg"
+      /></a>
+      
+      <a href="https://instagram.com/walrusz/" target="_blank"
+        ><img height="40" src="~/assets/svgs/instagram_logo.svg"
+      /></a>
+      <a
+        ><img height="40" class="avatar" src="~/assets/avatars/walrusz.webp"
+      /></a>
     </div>
   </v-app-bar>
 </template>
 
 <script>
 export default {
-    // eslint-disable-next-line vue/require-prop-types
-    props: ['user'],
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-    }
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['user'],
+  data() {
+    return {}
+  },
+  methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
 .center {
   margin: auto;
-  border: 3px solid green;
   padding: 10px;
   img {
-      margin-left: 5px;
-      margin-right: 5px;
+    margin-left: 5px;
+    margin-right: 5px;
   }
   .avatar {
-      border-radius: 50%;
+    border-radius: 50%;
   }
 }
 </style>
