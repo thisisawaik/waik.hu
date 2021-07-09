391eacebafad2478289a11fb2e2617b448d96c97
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-09 21:24:06 +0200

    Update walrusz.vue

--- web-vue/pages/profiles/walrusz.vue
+++ web-vue/pages/profiles/walrusz.vue
@@ -1,26 +1,8 @@
 <template>
-  <v-container>
-    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-      <template slot="progress">
-        <v-progress-linear
-          color="primary"
-          height="10"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-      <v-card-title class="white--text mt-8">
-        <v-avatar size="56"> <img alt="user" :src="imageurl" /></v-avatar>
-
-        <v-card-title>{{ name }}</v-card-title>
-      </v-card-title>
-
-      <v-card-text>
-        <div>
-          {{ desc }}
-        </div>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  <div>
+    <social-bar user="walrusz"></social-bar>
+    <description-card style="margin-top: 30px;" :desc="desc" :imageurl="imageurl" :name="name"></description-card>
+  </div>
 </template>
 
 <script>
@@ -29,7 +11,7 @@ export default {
     return {
       name: 'Walrusz',
       imageurl:
-        'https://yt3.ggpht.com/ytc/AAUvwniseLALwR5fmrqhrt2YDGgh1VJSCviENV2MkukhUg=s68-c-k-c0x00ffffff-no-rj',
+        'https://cdn.discordapp.com/avatars/183302720030113792/4fecad104687bc2c889bda7043276a6c.webp',
       dctag: 'Walrusz#1019',
       color: '#7f5b26',
       desc: `Üdvözöllek! Walrusz vagyok és úgy hiszem, bármiről lehet érdekes, kreatív és szórakoztató videót készíteni, hogyha az ember elegendő munkát fektet bele. Mesterségem elsősorban a szórakoztató ismeretterjesztő tartalmak gyártása, de emellett sok más mindent is találhatsz a csatornámon. Szeretek nagy hangsúlyt fektetni a videók vágására, az utómunkára és hiszek abban, hogy a minőségi szórakoztatás kulcsa a kreativitásban rejlik. Bátran nézz szét a csatornámon, háthatalálsz valamit, ami tetszik :)`,
