90590adcd46727a553b407e4c54cfefe4bd8e666
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-18 23:05:35 +0200

    Create CurrentEvetCard.vue

---
+++ web/components/CurrentEvetCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card max-width="374" elevation="2">
+    <div  v-html="$md.render(model)"></div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: this.$store.state.mdtest,
+    }
+  },
+}
+</script>
