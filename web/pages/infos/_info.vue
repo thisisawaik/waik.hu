01d1271fcb5b0d66f84bf2d8599e15c8df79db44
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-19 21:38:01 +0200

    Create _info.vue

---
+++ web/pages/infos/_info.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+		<template slot="progress">
+      <v-progress-linear
+      :color="`#${article.colorTheme}`"
+      value="100"
+      ></v-progress-linear>
+    </template>
+		{{ article.colorTheme }}
+    <article>
+      <h1>{{ article.title }}</h1>
+      <nuxt-content class="document" :document="article" />
+      <p class="updated">Frissítve: {{ formatDate(article.updatedAt) }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('infos', params.info).fetch()
+    return { article }
+  },
+  head() {
+    return {
+      title: `Waik | ${this.article.title}`,
+      meta: [
+        {
+          name: 'description',
+          content: this.article.description,
+        },
+        {
+          name: 'og:description',
+          content: this.article.description,
+        },
+        {
+          name: 'og:title',
+          content: `Waik | ${this.article.title}`,
+        },
+        {
+          name: 'og:image',
+          content: `${this.article.img}`,
+        },
+        {
+          name: 'og:image:alt',
+          content: `${this.article.alt}`,
+        },
+        {
+          name: 'theme-color',
+          content: `#${this.article.colorTheme}`,
+        },
+        {
+          name: 'article:modified_time',
+          content: this.article.updatedAt,
+        },
+        {
+          name: 'og:modified_time',
+          content: this.article.updatedAt,
+        },
+      ],
+    }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('hu', options)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.document h2 {
+  font-weight: bold;
+  font-size: 28px;
+}
+.document h3 {
+  font-weight: bold;
+  font-size: 22px;
+}
+.document p {
+  margin-bottom: 20px;
+}
+.document li {
+  margin-top: 20px;
+}
+.updated {
+  margin-top: 40px;
+}
+</style>
+
