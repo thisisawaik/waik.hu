2c7aaa633fa365e8e3c0f35516c7e5b748d8f03d
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-21 23:54:51 +0200

    Remove angular app

--- web-old/src/app/auto-form.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoFormDirective } from './auto-form.directive';
-
-describe('AutoFormDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoFormDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- web-old/src/app/dropzone.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { DropzoneDirective } from './dropzone.directive';
-import { MessagesService } from './services/messages.service';
-
-describe('DropzoneDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DropzoneDirective();
-    expect(directive).toBeTruthy();
-  });
-});
