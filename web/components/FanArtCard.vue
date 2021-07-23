4484956ccff3c288621ceaccf7ddb966bf054edd
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-23 21:48:42 +0200

    Update FanArtCard.vue

--- web/components/FanArtCard.vue
+++ web/components/FanArtCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+  <v-card :loading="loading" class="mx-auto my-12" elevation="15" max-width="374">
     <template slot="progress">
       <v-progress-linear
         color="deep-purple"
