1bd0f72e01a69ef3ad659a2cfc4924a03a1aaa85
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-27 20:14:20 +0200

    Update streams.vue

--- web/pages/streams.vue
+++ web/pages/streams.vue
@@ -1,5 +1,29 @@
 <template>
-  <p>asd</p>
+  <v-row no-gutters>
+    <template v-for="n in 4">
+      <v-col :key="n">
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <iframe
+            src="https://player.twitch.tv/?channel=isti115&parent=localhost"
+            frameborder="0"
+            allowfullscreen="true"
+            scrolling="no"
+            class="player"
+            width="100%"
+          />
+        </v-card>
+      </v-col>
+      <v-responsive
+        v-if="n === 2"
+        :key="`width-${n}`"
+        width="100%"
+      />
+    </template>
+  </v-row>
 </template>
 
 <script>
@@ -12,3 +36,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.player {
+  height: calc(100vh - 30px);
+}
+</style>
