e23bf20206f49948e9fa30ca7f04241afced04dd
Zalán Horváth <horvathzalan@zal1000.com>
2021-07-30 04:38:37 +0200

    Update _id.vue

--- web/pages/shares/_id.vue
+++ web/pages/shares/_id.vue
@@ -20,7 +20,7 @@ export default {
     if (doc.data().type === 'fanart') {
       const artref = db.collection('waik/website/fanarts').doc(doc.data().id)
       const doc2 = await artref.get()
-      console.log(doc2.data())
+      // console.log(doc2.data())
       if (doc2.data().getFromGS) {
         const sref = storage.ref(doc2.data().gsURL)
         const url = await sref.getDownloadURL()
@@ -30,7 +30,7 @@ export default {
       }
       title = doc2.data().title
       desc = doc2.data().desc
-      color = doc2.data().color || '#fff'
+      color = doc2.data().color || '#ffffff'
     }
     return {
       title,
@@ -58,7 +58,11 @@ export default {
           content: this.title
         },
         {
-          name: 'og:desc',
+          name: 'og:descriprion',
+          content: this.desc
+        },
+        {
+          name: 'descriprion',
           content: this.desc
         },
         {
