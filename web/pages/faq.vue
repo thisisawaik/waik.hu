410f2a895c0667fa4b3f3a58f6ec6a5c0b1334a5
Zalán Horváth <horvathzalan@zal1000.com>
2021-08-15 17:08:47 +0200

    Update faq.vue

--- web/pages/faq.vue
+++ web/pages/faq.vue
@@ -1,19 +1,23 @@
 <template>
   <v-flex>
     <v-container>
-      <v-expansion-panels>
-        <v-expansion-panel
-          v-for="(item, index) in faq"
-          :key="index"
-        >
-          <v-expansion-panel-header>
-            {{ item.q }}
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <nuxt-content :document="item" />
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <div v-for="category of contents" :key="category.key">
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(item, index) in category"
+            :key="index"
+          >
+            <v-expansion-panel-header>
+              {{ item.q }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <nuxt-content :document="item" />
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+        <br>
+        <br>
+      </div>
     </v-container>
   </v-flex>
 </template>
@@ -23,9 +27,31 @@ import Vue from 'vue'
 
 export default Vue.extend({
   async asyncData ({ app }) {
-    const faq = await app.$content(`${app.i18n.localeProperties.code}/infos/faq`).fetch()
+    const faq = await app.$content(`${app.i18n.localeProperties.code}/infos/faq`).where({ show: true }).sortBy('index').fetch()
+    const config = await app.$content(`${app.i18n.localeProperties.code}/infos/faq`, 'config').fetch()
+    const contents = []
+    for (const category of config.categorys) {
+      console.log(category)
+      const a = []
+      for (const item of faq) {
+        // eslint-disable-next-line no-mixed-operators
+        if (!item.category && category.key === 'default') {
+          a.push(item)
+        } else if (item.category === category.key) {
+          a.push(item)
+        }
+      }
+      console.log(a)
+      contents.push(a)
+    }
+    console.log(config)
     // console.log(faq)
-    return { faq }
+    console.log(contents)
+    return {
+      faq,
+      contents,
+      categorys: config.categorys,
+    }
   },
   data () {
     return {
