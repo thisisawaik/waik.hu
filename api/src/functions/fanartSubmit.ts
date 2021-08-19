f4f4ac1e864b86827cd6681d869e3ce56114c620
Zalán Horváth <horvathzalan@zal1000.com>
2021-08-19 22:22:53 +0200

    Update fanartSubmit.ts

--- api/src/functions/fanartSubmit.ts
+++ api/src/functions/fanartSubmit.ts
@@ -9,6 +9,11 @@ export default async function (rid: string, user: auth.UserRecord): Promise<fire
     if (!user) {
         throw new Error('user must be provided');
     }
+    const userref = db.collection('users').doc(user.uid);
+    const userDoc = await userref.get();
+    if (!userDoc.data()?.dcid) {
+        throw new Error('dc-not-linked');
+    }
     const collRef = db.collection('waik/website/fanarts')
     const oriRef = collRef.doc(id);
     const oriDoc = await oriRef.get();
@@ -19,7 +24,7 @@ export default async function (rid: string, user: auth.UserRecord): Promise<fire
         throw new Error('doc empty');
     }
 
-    const artsByUser = await db.collection('waik/website/fanarts').where('author', '==', user.uid).get();
+    const artsByUser = await db.collection('waik/website/fanarts').where('author', '==', user.uid).where('forComp', '==', true).get();
 
     if (artsByUser.docs.length > 3) {
         throw new Error('user has more than 3 fanarts');
